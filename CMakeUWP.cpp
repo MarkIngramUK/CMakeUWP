627b3b68ddbdea0bdf3cadba45bbb7404a13225a
Mark Ingram <mingram@serif.com>
2018-05-03 08:00:40 +0100

    Initial

---
+++ CMakeUWP.cpp
@@ -0,0 +1,40 @@
+﻿#include <winrt/Windows.ApplicationModel.Activation.h>
+#include <winrt/Windows.UI.h>
+#include <winrt/Windows.UI.Xaml.h>
+#include <winrt/Windows.UI.Xaml.Controls.h>
+#include <winrt/Windows.UI.Xaml.Media.h>
+
+using namespace winrt;
+using namespace winrt::Windows::ApplicationModel::Activation;
+using namespace winrt::Windows::UI;
+using namespace winrt::Windows::UI::Xaml;
+using namespace winrt::Windows::UI::Xaml::Controls;
+using namespace winrt::Windows::UI::Xaml::Media;
+
+namespace
+{
+	struct App : ApplicationT<App>
+	{
+		void OnLaunched(LaunchActivatedEventArgs const &)
+		{
+			TextBlock block;
+
+			block.FontFamily(FontFamily(L"Segoe UI Semibold"));
+			block.FontSize(72.0);
+			block.Foreground(SolidColorBrush(Colors::Orange()));
+			block.VerticalAlignment(VerticalAlignment::Center);
+			block.TextAlignment(TextAlignment::Center);
+			block.Text(L"Hello World!");
+
+			Window window = Window::Current();
+			window.Content(block);
+			window.Activate();
+		}
+	};
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
+{
+	init_apartment();
+	Application::Start([](auto &&) { make<App>(); });
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# CMakeUWP
+Minimal project for creating a UWP application using CMake build system
+
+## Requirements
+* Windows 10 April 2018
+* Windows SDK 10 (10.0.17134)
+* CMake 3.8
+
+For more information, see https://blogs.windows.com/buildingapps/2018/04/30/start-developing-on-windows-10-april-2018-update-today/.
